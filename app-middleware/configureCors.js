afcbafe6fa66a60dd63418125ac6d1cd19a6bae1
Jeremiah Loveland <jeremiah.loveland@gmail.com>
2018-04-03 15:28:30 -0400

    Fix issues with spacing and line endings

--- app-middleware/configureCors.js
+++ app-middleware/configureCors.js
@@ -3,15 +3,15 @@
 const cors = require('cors');
 
 function configure(app) {
-    app.use(cors({
-        origin: [
-            'http://127.0.0.1:3000',
-            'http://localhost:3000',
-        ],
-        methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-        allowedHeaders: ['Content-Type', 'Authorization', 'hostsuffix', 'Content-Disposition'],
-        preflightContinue: true,
-    }));
+  app.use(cors({
+    origin: [
+      'http://127.0.0.1:3000',
+      'http://localhost:3000',
+    ],
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization', 'hostsuffix', 'Content-Disposition'],
+    preflightContinue: true,
+  }));
 }
 
 module.exports = configure;
