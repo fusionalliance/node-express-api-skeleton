e7ee086c7db27f981ba0aa3006bdee2bbaac3ead
Jeremiah Loveland <jeremiah.loveland@gmail.com>
2018-04-04 17:36:21 -0400

    Alphabetized environment variables

--- src/config.js
+++ src/config.js
@@ -1,11 +1,12 @@
 'use strict';
 
 const config = {
+  allowedHeaders: process.env.ALLOWED_HEADERS || '*',
+  allowedOrigins: process.env.ALLOWED_ORIGINS || '*',
   appName: process.env.APP_NAME,
   debug: process.env.DEBUG,
   desiredPort: process.env.PORT || '3000',
-  allowedOrigins: process.env.ALLOWED_ORIGINS || '*',
-  allowedHeaders: process.env.ALLOWED_HEADERS || '*',
+  nodeEnv: process.env.NODE_ENV || 'development',
 };
 
 module.exports = config;
