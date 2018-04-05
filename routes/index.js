685a2210545324f185b745c67a01d1e4d0785fef
Jeremiah Loveland <jeremiah.loveland@gmail.com>
2018-04-05 16:16:11 -0400

    Moved swagger documentation to index

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,45 @@ const express = require('express');
 
 const router = new express.Router();
 
+/**
+ * @swagger
+ * definitions:
+ *  apiInfo:
+ *    properties:
+ *      title:
+ *        type: string
+ *        description: The title of the API
+ *      environment:
+ *        type: string
+ *        description: The environment
+ *      version:
+ *        type: string
+ *        description: The version of the API
+ *      build:
+ *        type: string
+ *        description: The build number
+ *      commit:
+ *        type: string
+ *        description: The commit hash
+ */
+
+/**
+ * @swagger
+ * /:
+ *  get:
+ *    tags:
+ *      - GetRoot
+ *    description: Returns information about the API
+ *    produces:
+ *      - application/json
+ *    responses:
+ *      200:
+ *        description: The API information
+ *        schema:
+ *          $ref: '#/definitions/apiInfo'
+ *
+ */
+
 router.use(require('./getRoot'));
 
 module.exports = router;
