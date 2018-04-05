5ad8875c5f72fdf95132b91750c0770470b5c393
Jeremiah Loveland <jeremiah.loveland@gmail.com>
2018-04-05 11:38:41 -0400

    Merged functions performing same work

--- app-middleware/configureCors.js
+++ app-middleware/configureCors.js
@@ -3,25 +3,17 @@
 const cors = require('cors');
 const config = require('../src/config');
 
-function parseOrigins(allowedOrigins) {
-  let origins = allowedOrigins;
-  if (allowedOrigins.indexOf(',') > -1) {
-    origins = allowedOrigins.split(',');
+function parseValues(originalValues) {
+  let parsedValues = originalValues;
+  if (originalValues.indexOf(',') > -1) {
+    parsedValues = originalValues.split(',');
   }
-  return origins;
-}
-
-function parseHeaders(allowedHeaders) {
-  let headers = allowedHeaders;
-  if (allowedHeaders.indexOf(',') > -1) {
-    headers = allowedHeaders.split(',');
-  }
-  return headers;
+  return parsedValues;
 }
 
 function configure(app) {
-  const allowedOrigins = parseOrigins(config.allowedOrigins);
-  const headers = parseHeaders(config.allowedHeaders);
+  const allowedOrigins = parseValues(config.allowedOrigins);
+  const headers = parseValues(config.allowedHeaders);
   app.use(cors({
     origin: allowedOrigins,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
