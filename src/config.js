6598d485ec9acab08f26be5672053b4e36ea14ba
Kory Marks <kory.marks@mac.com>
2018-04-09 14:47:45 -0400

    remove default values from config #7

--- src/config.js
+++ src/config.js
@@ -6,33 +6,31 @@ const config = cfg({
   allowedOrigins: {
     env: 'ALLOWED_ORIGINS',
     type: 'string',
-    default: '*',
+    required: true,
   },
   allowedHeaders: {
     env: 'ALLOWED_HEADERS',
     type: 'string',
-    default: '*',
+    required: true,
   },
   appName: {
     env: 'APP_NAME',
     type: 'string',
-    default: 'fusion-node-express-api-skeleton',
+    required: true,
   },
   desiredPort: {
     env: 'PORT',
     type: 'integer',
-    default: '3000',
+    required: true,
   },
   debug: {
     env: 'DEBUG',
     type: 'string',
-    default: 'fusion-node-express-api-skeleton:*',
   },
   nodeEnv: {
     env: 'NODE_ENV',
     type: 'enum',
     values: ['development', 'production'],
-    default: 'development',
   },
 });
 
