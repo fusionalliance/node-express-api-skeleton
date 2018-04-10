7317b0e72f7ee146bccb05ce5fdfcc75f66010cc
Jeremiah Loveland <jeremiah.loveland@gmail.com>
2018-04-10 13:43:48 -0400

    Adding a secret

--- src/config.js
+++ src/config.js
@@ -26,6 +26,11 @@ const config = cfg({
     env: 'DEBUG',
     type: 'string',
   },
+  topSecret: {
+    env: 'TOP_SECRET',
+    type: 'string',
+    required: true,
+  },
 });
 
 module.exports = config;
