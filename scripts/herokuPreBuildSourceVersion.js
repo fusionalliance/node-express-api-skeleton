dc947fd8f804f0db42ab0500df72d9d1d0572e1e
Kory Marks <kory.marks@mac.com>
2018-04-13 16:16:51 -0400

    fixing scripts and adding async

--- scripts/herokuPreBuildSourceVersion.js
+++ scripts/herokuPreBuildSourceVersion.js
@@ -3,7 +3,7 @@
 const exec = require('child_process').exec; // eslint-disable-line prefer-destructuring
 
 /* eslint-disable no-console */
-exec('sh sourceVersion.sh', (error, stdout, stderr) => {
+exec('sh ./scripts/sourceVersion.sh', (error, stdout, stderr) => {
   console.log(`${stdout}`);
   console.log(`${stderr}`);
   if (error !== null) {
