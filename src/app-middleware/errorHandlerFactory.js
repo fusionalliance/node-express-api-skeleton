4eb56a1dc273c64c9a7a19232ae5012c6cb606d7
Chad Allen Moore <mr.chad.moore@gmail.com>
2018-06-11 11:21:20 -0400

    updated errorHandlerFactory to return a middleware function, instead of an Express Router object

--- src/app-middleware/errorHandlerFactory.js
+++ src/app-middleware/errorHandlerFactory.js
@@ -1,30 +1,29 @@
 'use strict';
 
-const { Router } = require('express');
 const errorTypes = require('../models/errorTypes');
 const logFunctionFactory = require('../services/logFunctionFactory');
 
 module.exports = function errorHandlerFactory() {
   const writeError = logFunctionFactory.getErrorLogger('errorHandler');
-  return Router() // eslint-disable-line consistent-return
-    .use((err, req, res, next) => {
-      writeError('Error:', err);
-      if (res.headersSent) {
-        next(err);
-      } else if (err.errorType === errorTypes.badRequest) {
-        res
-          .status(400)
-          .json({ message: err.message });
-      } else if (err.errorType === errorTypes.notFound) {
-        res
-          .status(404)
-          .json({ message: err.message });
-      } else if (err.errorType === errorTypes.loginFailed) {
-        res
-          .status(401)
-          .json({ message: 'The username or password was invalid.' });
-      } else {
-        next(err);
-      }
-    });
+
+  return (err, req, res, next) => {
+    writeError('In custom error handler:', err);
+    if (res.headersSent) {
+      next(err);
+    } else if (err.errorType === errorTypes.badRequest) {
+      res
+        .status(400)
+        .json({ message: err.message });
+    } else if (err.errorType === errorTypes.loginFailed) {
+      res
+        .status(401)
+        .json({ message: 'The username or password was invalid.' });
+    } else if (err.errorType === errorTypes.notFound) {
+      res
+        .status(404)
+        .json({ message: err.message });
+    } else {
+      next(err);
+    }
+  };
 };
